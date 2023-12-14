24d9e9b421f90a0ee0519e6134ae27c54643fa8e
Severin Ibarluzea <seveibar@gmail.com>
2023-12-14 09:07:43 -0800

    v0.0.11

--- lib/get-seam.ts
+++ lib/get-seam.ts
@@ -5,9 +5,14 @@ import { getServer } from "./get-server"
 export const getSeam = () => {
   const config = getConfigStore()
 
+  const token = config.get(`${getServer()}.pat`)
+  const isPat = token.startsWith("seam_at")
+
   return new SeamHttp({
     endpoint: getServer(),
-    personalAccessToken: config.get(`${getServer()}.pat`),
+
+    personalAccessToken: isPat ? token : undefined,
+    apiKey: isPat ? undefined : token,
     // https://github.com/seamapi/javascript-http/issues/30
     workspaceId: config.get("current_workspace_id"),
   })

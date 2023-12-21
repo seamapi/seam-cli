5eb90449b33313b54605856098417501b7714bf0
Evan Sosenko <razorx@evansosenko.com>
2023-12-21 15:09:20 -0600

    Remove token_type

--- lib/get-seam.ts
+++ lib/get-seam.ts
@@ -7,21 +7,15 @@ export const getSeam = async (): Promise<SeamHttp> => {
 
   const token = config.get(`${getServer()}.pat`)
 
-  const token_type = token.startsWith("seam_at")
-    ? "pat"
-    : token.startsWith("ey")
-      ? "console-session-token"
-      : null
-
   const workspaceId = config.get("current_workspace_id")
 
   const options = { endpoint: getServer() }
 
-  if (token_type === "pat") {
+  if (token.startsWith("seam_at")) {
     return SeamHttp.fromPersonalAccessToken(token, workspaceId, options)
   }
 
-  if (token_type === "console-session-token") {
+  if (token.startsWith("ey")) {
     return SeamHttp.fromConsoleSessionToken(token, workspaceId, options)
   }
 

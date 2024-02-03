68ebcb3df903dc24bac8d8fde024f0d5ea6c4e40
kainpets <pawelstepniak22@gmail.com>
2024-02-03 18:30:26 +0100

    add the ability to select fakeseamconnect as server in interactive mode

--- lib/interact-for-server-selection.ts
+++ lib/interact-for-server-selection.ts
@@ -1,8 +1,15 @@
 import prompts from "prompts"
 import { getConfigStore } from "./get-config-store"
+import { randomBytes } from "node:crypto"
+import { getServer } from "./get-server"
 
 export async function interactForServerSelection() {
-  const servers = ["http://localhost:3020", "https://connect.getseam.com"]
+  const randomstring = randomBytes(5).toString("hex")
+  const servers = [
+    "http://localhost:3020",
+    "https://connect.getseam.com",
+    `https://${randomstring}.fakeseamconnect.seam.vc`,
+  ]
 
   const { server } = await prompts([
     {
@@ -14,7 +21,12 @@ export async function interactForServerSelection() {
   ])
 
   const config = getConfigStore()
-  config.set("server", server)
-  config.delete("current_workspace_id")
-  console.log(`Server set to ${server}`)
+  if (server === servers[2]) {
+    config.set(`${getServer()}.pat`, `seam_apikey1_token`)
+    console.log(`PAT set to use fakeseamconnect with "seam_apikey1_token"`)
+  } else {
+    config.set("server", server)
+    config.delete("current_workspace_id")
+    console.log(`Server set to ${server}`)
+  }
 }

65ac8b1b5e54e06031097e2e883f65b11e01c1ba
kainpets <pawelstepniak22@gmail.com>
2024-02-08 02:04:38 +0100

    wip: allow for fakeseamconnect selection

--- lib/interact-for-server-selection.ts
+++ lib/interact-for-server-selection.ts
@@ -1,15 +1,8 @@
 import prompts from "prompts"
 import { getConfigStore } from "./get-config-store"
-import { randomBytes } from "node:crypto"
-import { getServer } from "./get-server"
 
 export async function interactForServerSelection() {
-  const randomstring = randomBytes(5).toString("hex")
-  const servers = [
-    "http://localhost:3020",
-    "https://connect.getseam.com",
-    `https://${randomstring}.fakeseamconnect.seam.vc`,
-  ]
+  const servers = ["http://localhost:3020", "https://connect.getseam.com"]
 
   const { server } = await prompts([
     {
@@ -21,12 +14,7 @@ export async function interactForServerSelection() {
   ])
 
   const config = getConfigStore()
-  if (server === servers[2]) {
-    config.set(`${getServer()}.pat`, `seam_apikey1_token`)
-    console.log(`PAT set to use fakeseamconnect with "seam_apikey1_token"`)
-  } else {
-    config.set("server", server)
-    config.delete("current_workspace_id")
-    console.log(`Server set to ${server}`)
-  }
+  config.set("server", server)
+  config.delete("current_workspace_id")
+  console.log(`Server set to ${server}`)
 }
